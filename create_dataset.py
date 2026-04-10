4c6dad38e173ad30ab9c2acf31b583b643653d33
MBD <divinemountso@gmail.com>
2026-04-10 22:52:34 +0300

    data processing

---
+++ create_dataset.py
@@ -0,0 +1,37 @@
+import mediapipe as mp
+import os
+import cv2
+import matplotlib.pyplot as plt
+
+
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_style = mp.solutions.drawing_styles
+
+hands = mp_hands.Hands(static_image_mode=True,min_detection_confidence=0.5)
+
+
+DATA_DIR = './data'
+
+for dir_ in os.listdir(DATA_DIR):
+    for img_path in os.listdir(os.path.join(DATA_DIR, dir_))[:1]:
+        img = cv2.imread(os.path.join(DATA_DIR, dir_,img_path))
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        results = hands.process(img_rgb)
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                img_rgb, #image to draw
+                hand_landmarks, #model output
+                mp_hands.HAND_CONNECTIONS, #hand connection
+                mp_drawing_style.get_default_hand_landmarks_style(),
+                mp_drawing_style.get_default_hand_connections_style()
+            )
+
+        plt.figure()
+        plt.imshow(img_rgb)
+
+plt.show()
+
+
